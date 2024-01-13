37f20f75c07ea69f8862d300aeb5a5dff57820c7
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 02:57:32 +0500

    NODEJS Assignments Q11
    11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.

---
+++ Q11.ts
@@ -0,0 +1,7 @@
+//10. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let names: string[] = ["Rizwan Akhtar", "Nida Rizwan", "Ahsen Rizwan", "Akhtar Hussain"];
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
